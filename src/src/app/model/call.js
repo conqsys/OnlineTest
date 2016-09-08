05cb084691aa88d16fa49941f11a18607800ee7c
amit.kumar <amit.kumar@9cada54f-4a83-7a42-817c-85762a3fc62f>
2016-09-08 09:44:09 +0000

    delete unused models

--- src/src/app/model/call.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var CallModel = (function () {
-    function CallModel() {
-    }
-    return CallModel;
-}());
-exports.CallModel = CallModel;
-//# sourceMappingURL=call.js.map
--- src/src/app/model/companyinfo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var ComopayInfoModel = (function () {
-    function ComopayInfoModel() {
-    }
-    return ComopayInfoModel;
-}());
-exports.ComopayInfoModel = ComopayInfoModel;
-//# sourceMappingURL=companyinfo.js.map

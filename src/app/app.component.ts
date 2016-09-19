4ffdfa0ac6c91c118fd1bc023da7d467ab5df645
amit.kumar <amit.kumar@9cada54f-4a83-7a42-817c-85762a3fc62f>
2016-09-19 09:51:47 +0000

    add page links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  template: `<router-outlet></router-outlet>`,
+  template: `<div class="col-md-12">
+      
+          <a class="btn" routerLink="/questions" routerLinkActive="active">Questions</a>
+          <a class="btn" routerLink="/questionsets" routerLinkActive="active">Question Sets</a>
+          <a class="btn" routerLink="/topiclist" routerLinkActive="active">Topic</a>
+          <a class="btn" routerLink="/companylist" routerLinkActive="active">Companies</a>
+          <a class="btn" routerLink="/users" routerLinkActive="active">Users</a>
+
+    </div>
+    <div class="col-md-12">
+    <router-outlet></router-outlet></div>`,
   styleUrls: ['app.component.css']
 })
 export class AppComponent {

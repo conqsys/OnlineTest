6b700bc448222ab62fc516549b4624f6717b48e8
Manoj <manoj@conqsys.com>
2016-10-12 15:01:10 +0530

    change Questionset and Question

--- sails-api/api/controllers/QuestionSetController.js
+++ sails-api/api/controllers/QuestionSetController.js
@@ -11,9 +11,9 @@ module.exports = {
         var companyId = req.param('company_id');
         QuestionSet.find({ company_id: companyId })
             .exec(function (err, result) {
-                if (err) { 
+                if (err) {
                     return res.serverError(err);
-                } else { 
+                } else {
                     return res.json(result);
                 }
             })
@@ -25,7 +25,7 @@ module.exports = {
 
         QuestionSet.findOne({ company_id: companyId, question_set_id: quesSetId })
             .exec(function (err, result) {
-                if (err) { 
+                if (err) {
                     return res.serverError(err);
                 } else {
                     var obj = result;
@@ -34,7 +34,7 @@ module.exports = {
 
                     var str = "CALL spGetQuestionSetQuestions(" + quesSetId + "," + companyId + ")";
                     QuestionSet.query(str, function (err, result) {
-                        if (err) { 
+                        if (err) {
                             return res.serverError(err);
                         } else {
                             if (result[0].length == 1 && result[0][0].question_set_id == null)
@@ -49,14 +49,22 @@ module.exports = {
             })
     },
     // get QuestionSets by user_id from  database
-    getQuestionSetsbyUser : function (req, res) {
+    getQuestionSetsbyUser: function (req, res) {
         var userId = req.param('user_id');
         var str = "CALL spGetQuestionSets(" + userId + ")";
         QuestionSet.query(str, function (err, result) {
-            if (err) { 
+            if (err) {
                 return res.serverError(err);
             } else {
-                 return res.json(result[0]);
+                for (var i = 0; i < result[0].length; i++) {
+                    result[0][i].test_completed_date = result[0][i].test_completed_date.getDate() + "-" + result[0][i].test_completed_date.getMonth() + "-" + result[0][i].test_completed_date.getFullYear();
+                    // result[i].test_end_date = result[i].test_end_date.getDate()+"/"+result[i].test_end_date.getMonth()+"/"+result[i].test_end_date.getFullYear();
+                    result[0][i].total_time = toSeconds(result[0][i].total_time);
+                    result[0][i].test_start_time = tConvert(result[0][i].test_start_time);
+                    result[0][i].test_end_time = tConvert(result[0][i].test_end_time);
+
+                }
+                return res.json(result[0]);
             }
         });
     },
@@ -76,7 +84,7 @@ module.exports = {
             + req.body.created_by + "','"
             + req.body.updated_by + "')";
         QuestionSet.query(str, function (err, result) {
-            if (err) { 
+            if (err) {
                 return res.serverError(err);
             } else {
                 var question_set_id = result[0][0].id;
@@ -91,7 +99,7 @@ module.exports = {
                         }
                         if (str != "") {
                             QuestionSet.query(str, function (err, result) {
-                                if (err) { 
+                                if (err) {
                                     return res.serverError(err);
                                 } else {
                                     if (i == question_set_questions.length - 1) {
@@ -113,3 +121,22 @@ module.exports = {
     }
 };
 
+function tConvert(time) {
+    // Check correct time format and split into components
+    time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+
+    if (time.length > 1) { // If time format correct
+        time = time.slice(1);  // Remove full string match value
+        time[3] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
+        time[0] = +time[0] % 12 || 12; // Adjust hours
+    }
+    return time.join(''); // return adjusted time or original string
+}
+function toSeconds(time_str) {
+    // Extract hours, minutes and seconds
+    var parts = time_str.split(':');
+    // compute  and return total seconds
+    return parts[0] * 60 + // an hour has 3600 seconds
+        parts[1] * 1; // a minute has 60 seconds
+       // parts[2]; // seconds
+}

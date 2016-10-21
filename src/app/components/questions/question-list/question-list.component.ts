b65dc625f401ebf1830961c5d3c1ccf264bf901d
conqsys <manoj@conqsys.com>
2016-10-21 11:56:58 +0530

    added animation in question list module

--- src/app/components/questions/question-list/question-list.component.ts
+++ src/app/components/questions/question-list/question-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input,  trigger, state, style, transition, animate,keyframes,SecurityContext,Sanitizer} from '@angular/core';
 import { Router} from '@angular/router';
 import { LocalStorageService } from 'angular-2-local-storage';
+
 
 import { BaseComponent } from '../../base.component';
 
@@ -15,6 +16,33 @@ import { QuestionOptionService } from '../../../shared/services/question-option/
   selector: 'app-questions',
   templateUrl: 'question-list.component.html',
   styleUrls: ['question-list.component.css'],
+   animations: [
+  trigger('flyInOut', [
+  
+    transition('* =>1', [
+      animate(2000, keyframes([
+        style({opacity: 0, transform: 'translateX(-100%)', offset: 0}),
+        style({opacity: 1, transform: 'translateX(100%)',  offset: 0.5}),
+        style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
+      ]))
+    ]),
+    transition('* =>2', [
+      animate(1000, keyframes([
+        style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+        style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
+        style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
+      ]))
+    ])
+    ,
+    transition('* =>3', [
+      animate(2500, keyframes([
+        style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+        style({opacity: 1, transform: 'translateX(-15px)', offset: 0.8}),
+        style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
+      ]))
+    ])
+  ])
+]
 })
 export class QuestionListComponent extends BaseComponent implements OnInit {
   private statInfo: StatInfo;
@@ -24,23 +52,26 @@ export class QuestionListComponent extends BaseComponent implements OnInit {
   constructor(private questionService: QuestionService,
     private questionOptionService: QuestionOptionService,
     localStorageService: LocalStorageService,
-    router: Router) {
+    router: Router,private sanitize:Sanitizer) {
     super(localStorageService, router);
     this.statInfo = new StatInfo();
     this.model = new Array<Question>();
     this.selectedQuestion = new Question();
     this.selectedQuestion.answer_explanation = '';
+    
   }
 
   ngOnInit(): void {
     if (this.user) {
       this.getQuestionStateInfo();
+    
     }
   }
 
   selectQuestion(selectedQuestion: Question): void {
     this.selectedQuestion = selectedQuestion;
     this.router.navigate(['/question', selectedQuestion.question_id]);
+    
   }
 
   addQuestion(): void {
